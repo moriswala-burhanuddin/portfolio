52024bc5448f5430bfa57d7dbe13ec3bf005e43c
moriswalab <burhanuddinmoris52@gmail.com>
2025-02-19 14:34:43 +0530

    Updated .gitignore to ignore Django backend files

--- burhanuddin/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Review, Contact  # Import your models
-
-# Register your models with the admin site
-admin.site.register(Review)
-admin.site.register(Contact)
